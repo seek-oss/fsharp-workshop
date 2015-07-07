2198ae52eb17dc1e53db455778134f581a35266d
Andrew Browne <brownie@brownie.com.au>
2015-07-07 23:23:34 +1000

    Added build script to run exercises

---
+++ build.fsx
@@ -0,0 +1,22 @@
+#r "packages/FAKE/tools/FakeLib.dll" // include Fake lib
+open Fake
+
+Target "Functions" (fun _ ->
+    let result, output = FSIHelper.executeFSI "./exercises" "functions.fsx" [] //[("use", "functions.fsx")]
+    for msg in output do
+      printfn "%s" msg.Message
+    ()
+)
+
+Target "Examples" (fun _ ->
+    trace "Testing stuff..."
+)
+
+Target "Deploy" (fun _ ->
+    trace "Heavy deploy action"
+)
+
+"Functions"            // define the dependencies
+   ==> "Examples"
+
+Run "Examples"
