780de7163fbcea277d223fee40a85f77c4672c2f
Simon Fenton <swfenton@gmail.com>
2015-07-08 11:32:06 +1000

    Add module name
    FSI was complaining about lack of module name

--- exercises/examples.fs
+++ exercises/examples.fs
@@ -1,10 +1,11 @@
-let test name f =
-  try 
-    match f () with
-    | true -> printf "%s: passed" name
-    | false -> printf "%s: failed" name
-  with
-    | e when e.Message = "todo" ->
-      printf "%s: TODO" name
-    | e ->
-      printf "%s: Exception %A" name e
+module Examples
+  let test name f =
+    try
+      match f () with
+      | true -> printf "%s: passed" name
+      | false -> printf "%s: failed" name
+    with
+      | e when e.Message = "todo" ->
+        printf "%s: TODO" name
+      | e ->
+        printf "%s: Exception %A" name e
