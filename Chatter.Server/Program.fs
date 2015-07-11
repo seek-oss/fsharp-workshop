c82305667501e601ae83c6e586e4be4a77bed115
Andrew Browne <brownie@brownie.com.au>
2015-07-11 18:09:45 +1000

    Basic suave sample

--- Chatter.Server/Program.fs
+++ Chatter.Server/Program.fs
@@ -1,8 +1,11 @@
 ﻿open Suave
+open Suave.Http.Successful
+open Suave.Web
+open Suave.Types
 // Learn more about F# at http://fsharp.net
 // See the 'F# Tutorial' project for more help.
 [<EntryPoint>]
 let main argv = 
     printfn "%A" argv
+    startWebServer defaultConfig (OK "Hello World!")
     0 // return an integer exit code
-
