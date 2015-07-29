de7197e56828d71b4b712fdaa4ab214b7869ea92
Andrew Browne <brownie@brownie.com.au>
2015-07-29 19:46:55 +1000

    Added profiles projects

---
+++ profiles/profiles.server/AssemblyInfo.fs
@@ -0,0 +1,18 @@
+﻿module profiles.server.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly:AssemblyTitle("profiles.server")>]
+[<assembly:AssemblyDescription("")>]
+[<assembly:AssemblyConfiguration("")>]
+[<assembly:AssemblyCompany("")>]
+[<assembly:AssemblyProduct("")>]
+[<assembly:AssemblyCopyright("andrew")>]
+[<assembly:AssemblyTrademark("")>]
+[<assembly:AssemblyVersion("1.0.0.0")>]
+()
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
---
+++ profiles/profiles.server/Program.fs
@@ -0,0 +1,5 @@
+﻿[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // return an integer exit code
+
