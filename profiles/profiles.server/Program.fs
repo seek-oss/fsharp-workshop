d62f82e25e595f71b5aa51dc8b9b1bea03ad84c8
Andrew Browne <brownie@brownie.com.au>
2015-07-29 20:44:40 +1000

    Profiles: basic structure in place

--- profiles/profiles.server/Program.fs
+++ profiles/profiles.server/Program.fs
@@ -1,5 +1,34 @@
-﻿[<EntryPoint>]
-let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+﻿namespace profiles.server
 
+open Suave
+open Suave.Types
+open Suave.Http
+open Suave.Http.Applicatives
+open Suave.Http.Successful
+open Suave.Http.Files
+open Suave.Web
+
+module Main =
+  [<CLIMutable>]
+  type ProfileForm = {
+    Name : string
+  }
+
+  let asJson (f : ('a -> Choice<'r, string list>)) : WebPart =
+    failwith "todo"
+
+  let persistProfile (form : ProfileForm) =
+    Choice2Of2 ["Didn't like it"]
+
+  let suaveApp =
+    choose
+      [ GET >>= choose
+         [ path "/" >>= file "./static/index.html"
+           pathScan "/static/%s" (sprintf "./static/%s" >> file) ]
+        PUT >>=
+           path "/profile" >>= asJson persistProfile ]
+
+  [<EntryPoint>]
+  let main argv =
+    startWebServer defaultConfig suaveApp
+    0
