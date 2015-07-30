565be186af73bed6f3732e9ae1bcbb0c996e4dfe
Jeremy Clough <jeremy@talesin.net>
2015-07-30 13:20:16 +1000

    splitting up list exercises

--- exercises/lists2.fsx
+++ exercises/lists2.fsx
@@ -1,69 +1,11 @@
 (**********************************************************************************************************************
-    Lists are a big part of functional programming langages and F# is no exception. You may be familar with List<T>
-    in C# however F# lists are quite different, essentially being based on good old dependable singly-linked lists (but
-    using tuples) whereas C# lists use arrays.
-
-    A naive implementation of an F# list could look like:
-
-    type List<'T> =
-    | ([]) of List<'T>
-    | (::) of Head: 'T * Tail: List<'T>
-
-    Head and tail.
-
-    Explain tuples
-
-    In F# an empty list is represented by [] and cons by the :: operator
+    In this section we'll go through implementing some of the common list functions that you would have come across
+    in the previous exercise.
 *)
 
 #load "./examples.fs"
 open Examples
 
-
-(**********************************************************************************************************************
-    Let's see some examples for constructing lists - copy the following into the REPL to see the result.
-*)
-
-let empty         = []
-
-let madeWithCons  = 1 :: 2 :: 3 :: 4 :: []
-
-let explicit      = [ 1; 2; 3; 4 ]
-
-let multiline     = [
-                      1
-                      2
-                      3
-                      4
-                    ]
-
-let range         = [ 1 .. 10 ]
-
-let rangeWithSkip = [ 1 .. 2 .. 10 ]
-
-let alphaRange    = [ 'a' .. 'z' ]
-
-let floatRange    = [ 0.1 .. 0.1 .. 1.0 ]
-
-
-(**********************************************************************************************************************
-    You can append to a list using the '@' symbol, however this iterates through the items in the first list before
-    it can return the newly combined list.
-*) 
-
-
-let appendedList  = [ 1; 2; 3 ] @ [ 4; 5; 6 ]
-
-(**********************************************************************************************************************
-    Another way to create lists is through list comprehensions.
-*) 
-
-
-let forLoop = [ for i in 1 .. 10 -> i * 2 ]
-
-// another example of for using do and yield.
-
-// some basic examples using List module functions with LINQ comparisons
 
 (**********************************************************************************************************************
     They can also contain functions and be fairly complex. Use F# Interactive to see the result of the list
@@ -507,12 +449,6 @@ test "Write a function to return the last item of list using reduce" (fun () ->
   last2 prime100 = 97
 )
 
-
-(**********************************************************************************************************************
-    Now you should have a reasonable understand of some common list functions, including the much hyped map reduce.
-
-
-*)
 
 // arrays
 // sequences
