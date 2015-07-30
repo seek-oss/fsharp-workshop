842a068037c55ff4cabf84bcdd63dc3b50dd11df
Jeremy Clough <jeremy@talesin.net>
2015-07-31 06:39:40 +1000

    lists - still editing

--- exercises/lists2.fsx
+++ exercises/lists2.fsx
@@ -1,11 +1,10 @@
 #load "./examples.fs"
 open Examples
-(**********************************************************************************************************************
-    A useful feature of lists is the ability to pattern match them. Using the cons and empty list symbols
-    you can match pretty much anywhere in a list.
-
-    The following two example shows some simple matching on a list by splitting between the head and the tail. 
-*)
+(*********************************************************************************************************************)
+//  A useful feature of lists is the ability to pattern match them. Using the cons and empty list symbols
+//  you can match pretty much anywhere in a list.
+//
+//  The following two example shows some simple matching on a list by splitting between the head and the tail. 
 
 let fibonacci = [0; 1; 1; 2; 3; 5; 8; 13; 21; 34; 55; 89]
 
@@ -30,10 +29,7 @@ let isLast =
   | x :: [] -> true
   | _       -> false
 
-
-(**********************************************************************************************************************
-    Using only a match expression, return the fourth item from the Fibonacci sequence (use fib25)
-*)
+// Using only a match expression, return the fourth item from the Fibonacci sequence (use fib25)
 
 // fourthFib: () -> int option
 let fourthFib () =
--- exercises/sequences1.fsx
+++ exercises/sequences1.fsx
@@ -4,3 +4,5 @@
 
 #load "./examples.fs"
 open Examples
+
+//unfold - let triangles = Seq.unfold (fun (a, b) -> Some(a + b, (a+1, a + b))) (1, 0)
