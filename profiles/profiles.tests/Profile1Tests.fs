212fbada96c4d172d02491c17b59f465522ca5a4
Andrew Browne <abrowne@seek.com.au>
2015-07-30 13:07:31 +1000

    Removed unquote

--- profiles/profiles.tests/Profile1Tests.fs
+++ profiles/profiles.tests/Profile1Tests.fs
@@ -1,38 +1,41 @@
 ﻿namespace profiles.tests
 open System
 open Xunit
-open Swensen.Unquote
 open profiles.server.Profile1
 
 module Profile1Tests = 
     [<Fact>]
     let ``A valid user is saved`` () =
-        test
-            <@ persistProfile {
-                    FirstName = "Foo"
-                    LastName = "Bar"
-               } = Choice1Of2 "Saved" @>
+        Assert.Equal(
+            persistProfile {
+                FirstName = "Foo"
+                LastName = "Bar"
+            },
+            Choice1Of2 "Saved")
 
     [<Fact>]
     let ``An empty FirstName returns error`` () =
-        test
-            <@ persistProfile {
-                    FirstName = ""
-                    LastName = "Bar"
-               } = Choice2Of2 ["Firstname is required"] @>
+        Assert.Equal(
+            persistProfile {
+                FirstName = ""
+                LastName = "Bar"
+            },
+            Choice2Of2 ["Firstname is required"])
 
     [<Fact>]
     let ``An empty LastName returns error`` () =
-        test
-            <@ persistProfile {
-                    FirstName = "Foo"
-                    LastName = ""
-            } = Choice2Of2 ["Lastname is required"] @>
+        Assert.Equal(
+            persistProfile {
+                FirstName = "Foo"
+                LastName = ""
+            },
+            Choice2Of2 ["Lastname is required"])
 
     [<Fact>]
     let ``All errors are returned`` () =
-        test
-            <@ persistProfile {
-                    FirstName = ""
-                    LastName = ""
-            } = Choice2Of2 ["Firstname is required"; "Lastname is required"] @>
+        Assert.Equal(
+            persistProfile {
+                FirstName = ""
+                LastName = ""
+            },
+            Choice2Of2 ["Firstname is required"; "Lastname is required"])
