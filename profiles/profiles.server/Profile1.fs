424ce1c883355c4e12998e3a75717cd226ada2f6
Andrew Browne <brownie@brownie.com.au>
2015-08-01 17:33:56 +1000

    Remove answers for profile

--- profiles/profiles.server/Profile1.fs
+++ profiles/profiles.server/Profile1.fs
@@ -22,35 +22,10 @@ module Profile1 =
     | "" -> (sprintf "%s is required" name)::errors
     | _ -> errors
 
-  let validatePostcode name (value : string) errors =
-      let checks = [
-        value.Length = 4 || value.Length = 0
-        Seq.forall Char.IsDigit value
-      ]
-
-      if checks |> Seq.forall id then
-        errors
-      else
-        (sprintf "%s must be 4 digits" name)::errors
-
-  let validateNoBadWords name (value : string) errors =
-    let hasBadWords =
-        value.Split(' ')
-        |> Seq.map (fun x -> x.ToLower())
-        |> Seq.map (fun x -> x |> Seq.filter Char.IsLetter |> String.Concat)
-        |> Seq.exists (fun word -> Set.contains word badWords)
-    if hasBadWords then
-        (sprintf "%s must not contain bad words" name)::errors
-    else
-        errors
-
   let persistProfile (form : ProfileForm) : SaveResult<string> =
     let errors =
         []
         |> requiredString "Firstname" form.Firstname
-        |> requiredString "Lastname" form.Lastname
-        |> validateNoBadWords "Description" form.Description
-        |> validatePostcode "Postcode" form.Postcode
 
     match errors with
     | [] ->
