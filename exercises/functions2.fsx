6919e1d5e606f1486d818af19ae3af15260933d3
Andrew Browne <brownie@brownie.com.au>
2015-08-02 11:56:51 +1000

    Simplified functions

---
+++ exercises/functions2.fsx
@@ -0,0 +1,23 @@
+// this file contains some extra
+// function examples. Try these if you are bored
+// by the first set
+#load "./examples.fs"
+
+// the next number in the fibonacci sequence is computed
+// by adding the previous two together
+// ie: 1, 1, 2, 3, 5
+// hint: 'rec' keyword allows you to call a function recursively
+// you'll probably need that for this exercise.
+let rec fib n =
+  match n with
+  | 0 -> 0
+  | 1 -> 1
+  | n -> (fib (n - 1)) + (fib (n - 2))
+
+Examples.test "Calculate the 10th fibonacci number" (fun () ->
+    fib 10 = 55
+)
+
+Examples.test "Calculate the 100th fibonacci number" (fun () ->
+    fib 15 = 610
+)
